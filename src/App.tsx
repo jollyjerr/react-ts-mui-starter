8c30efa3cddc296c5bd0c23387f7f2afbb30fc4d
jollyjerr <jollyjerr@gmail.com>
2020-09-01 21:26:58 -0600

    add style setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./assets/theme";
 
 const App = () => {
-    return <div></div>;
+    return (
+        <ThemeProvider theme={theme}>
+            <div>Hi</div>
+        </ThemeProvider>
+    );
 };
 
 export default App;
